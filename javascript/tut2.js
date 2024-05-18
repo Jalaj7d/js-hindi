f2fd3b1b78b65256a75703f273c698cc89cee713
Jalaj Dubey <116143485+Jalaj7d@users.noreply.github.com>
2024-05-18 08:20:15 +0000

    datatypes

---
+++ javascript/tut2.js
@@ -0,0 +1,11 @@
+const aid = 2345
+let accemail = "jalajdubey7@gmail.com"
+var accpass = "5243"
+acccity = "Mds"
+
+accemail = "jalajdubey7@gmail.com"
+accpass = "5243"
+acccity = "Mds"
+
+console.log(aid);
+console.table([aid,accemail,accpass,acccity])
---
+++ javascript/tut3.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using nodejs not browser
+let name = "Jalaj"
+let age = 22
+let boole = true
+
+// number - 2^53
+// bigint
+// string - ""
+// boolean - true/false
+// null - standalone value
+// undefined
+// symbol - unique
+
+// object
+
+console.log(typeof age)
+console.log(typeof "jalaj")
+console.log(typeof null)
+console.log(typeof undefined)
