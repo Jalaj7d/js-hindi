8f4edca84d1e9b7a78d976f5c5b1538a4cdcc6d1
Jalaj Dubey <116143485+Jalaj7d@users.noreply.github.com>
2024-05-19 11:21:08 +0000

    conversion

---
+++ javascript/tut4 (conversion).js
@@ -0,0 +1,38 @@
+// let s = "23"
+// let s1 = "33abc"
+// console.log(typeof s)
+// console.log(typeof (s))
+
+// let vin = Number(s)
+// console.log(typeof (vin))
+
+// let vin1 = Number(s1)
+// console.log(typeof (vin1))
+// console.log(vin1)
+
+/* 
+"33" - 33
+"33abc" - NaN (Not a Number)
+null - 0
+undefined - NaN
+true - 1 and false - 0
+"jalaj" - NaN
+*/
+
+// let ili = 0
+
+// let bil = Boolean(ili)
+// console.log(bil)
+
+/*
+1 - true
+0 - false
+"" - false
+"jalaj" - true
+*/
+
+let sn = 33
+
+let stn = String(sn)
+console.log(stn)
+console.log(typeof stn)
