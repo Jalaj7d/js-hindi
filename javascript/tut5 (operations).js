b97105c8a082e2190b9dbe373b3a4906ca2e23bd
Jalaj Dubey <116143485+Jalaj7d@users.noreply.github.com>
2024-05-20 15:41:28 +0000

    opertions

---
+++ javascript/tut5 (operations).js
@@ -0,0 +1,31 @@
+let v = 3
+let nv = -v
+// console.log(nv)
+
+// console.log(2+3)
+// console.log(2-7)
+// console.log(2*5)
+// console.log(6/2)
+// console.log(2**3)
+// console.log(7%2)
+
+let s1 = "Hello"
+let s2 = " Jalaj"
+
+let s3 = s1+s2
+// console.log(s3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true) // but true+ is not acceptable
+// console.log(+"")
+
+let n1, n2, n3
+n1 = n2 = n3 = 2+2
+
+let gc = 100
+++gc
+console.log(gc)
